e234fd51784edc6ed09550a3fd033da5fefbde25
Gniare <niare09@hotmail.fr>
2024-04-08 16:30:08 +0200

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
  * binary_tree_leaves - Counts the leaves in a binary tree
@@ -9,34 +9,33 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    const binary_tree_t *current;
-    size_t leaves = 0;
-    binary_tree_t **stack = NULL;
-    size_t stack_size = 0;
-
     if (tree == NULL)
         return 0;
 
-    stack = malloc(sizeof(binary_tree_t *) * 1000);  // Assuming a maximum stack size of 1000
+    size_t leaves_count = 0;
+    const binary_tree_t *current_node = NULL;
+    binary_tree_t **stack = malloc(sizeof(binary_tree_t *) * 1024); // Assuming max size of the stack
+    size_t top = 0;
+
     if (stack == NULL)
         return 0;
 
-    stack[stack_size++] = (binary_tree_t *)tree;
+    stack[top++] = (binary_tree_t *)tree;
 
-    while (stack_size > 0)
+    while (top > 0)
     {
-        current = stack[--stack_size];
+        current_node = stack[--top];
+        
+        if (current_node->left == NULL && current_node->right == NULL)
+            leaves_count++;
 
-        if (current->left == NULL && current->right == NULL)
-            leaves++;
+        if (current_node->right != NULL)
+            stack[top++] = current_node->right;
 
-        if (current->left != NULL)
-            stack[stack_size++] = (binary_tree_t *)current->left;
-
-        if (current->right != NULL)
-            stack[stack_size++] = (binary_tree_t *)current->right;
+        if (current_node->left != NULL)
+            stack[top++] = current_node->left;
     }
 
     free(stack);
-    return leaves;
+    return leaves_count;
 }
