5574aaa59c356c3053a97fc062a9cf937eceab67
Edouard26 <edouard.halimi@gmail.com>
2024-04-09 15:07:15 +0200

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,11 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	/* Recursively measure the left and right subtree heights */
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	/* Return the maximum height between left and right subtrees, plus 1 for the current level */
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
-
